c43973380c1e5e01e5ac4cbe0c6c857de10faff6
bbarber <bbarber@coastalcomputerservices.net>
2015-11-11 15:16:46 -0500

    schema.rb updated to reflect new foreign key for user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151107141006) do
+ActiveRecord::Schema.define(version: 20151110224134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,10 @@ ActiveRecord::Schema.define(version: 20151107141006) do
     t.datetime "updated_at",    null: false
     t.string   "house_mgmt_co"
     t.string   "manager"
+    t.integer  "user_id"
   end
+
+  add_index "properties", ["user_id"], name: "index_properties_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
@@ -63,4 +66,5 @@ ActiveRecord::Schema.define(version: 20151107141006) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["token"], name: "index_users_on_token", unique: true, using: :btree
 
+  add_foreign_key "properties", "users"
 end
